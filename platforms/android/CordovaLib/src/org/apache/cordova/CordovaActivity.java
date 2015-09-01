3e07ff1e31eb13c8d09a9afddf110cff04024f77
daddy <daddy@biddie.io>
2015-09-01 03:18:42 -0400

    Get rid of icon copying-- it ruins everything

--- platforms/android/CordovaLib/src/org/apache/cordova/CordovaActivity.java
+++ platforms/android/CordovaLib/src/org/apache/cordova/CordovaActivity.java
@@ -111,14 +111,6 @@ public class CordovaActivity extends Activity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
 
         super.onCreate(savedInstanceState);
 
