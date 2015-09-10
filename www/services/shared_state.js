fcd52ba8bbcccb1b0ca64f435dedf4777a899a6d
daddy <daddy@biddie.io>
2015-09-09 20:15:38 -0400

    add DC as a state

--- www/services/shared_state.js
+++ www/services/shared_state.js
@@ -29,7 +29,7 @@ angular.module('foodmeApp.sharedState', [])
     // token isn't set. Setting this to false would disable this behavior for
     // testing purposes.
     possibleStates: [
-      'AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA',
+      'AL','AK','AZ','AR','CA','CO','CT','DC','DE','FL','GA',
       'HI','ID','IL','IN','IA','KS','KY','LA','ME','MD',
       'MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ',
       'NM','NY','NC','ND','OH','OK','OR','PA','RI','SC',
